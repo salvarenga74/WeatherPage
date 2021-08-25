ee07b3337a6addf3c05795b7375eb3b0a8b8f6d0
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-24 20:44:55 -0500

    API for 5 day forcast works well and im setting the innerHTML to the main box

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,28 @@
-let apiKey = "650a47000924349e89d41d97ff6cc517";
-let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=${apiKey}`;
 
-let searchBarEl = document.querySelector("#searchCityBar");
-let searchVal = "";
+
+
 let searchBtnEl = document.querySelector(".searchBtn");
 
+searchBtnEl.addEventListener("click", whatCity);
 
+function whatCity(event){
+    event.preventDefault()
+    let searchBarEl = document.querySelector("#searchCityBar");
+    let searchVal = searchBarEl.value;
 
-function whatCity(){
-    let searchVal = searchBarEl.nodeValue;
-}
+    weatherInfo(searchVal);
+};
+change 
+function weatherInfo(city){
+   console.log(city)
+    let apiKey = "650a47000924349e89d41d97ff6cc517";
+    let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
-let weatherInfo = function(event){
     fetch(queryUrl)
-    .then(function (response){
-        console.log("response: " + response);
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    
-// .then(function(city){
-    
-// })
-}
+        .then(headers => headers.json())
+        .then(weatherData => {
+            console.log(weatherData);
+            document.querySelector("#currentPlace").innerHTML = weatherData["city"]["name"];
 
-
-
-searchBtnEl.addEventListener("click", weatherInfo)
+        })
+    }
